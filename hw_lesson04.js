29343d6751fcd640f56354beba0e296c5f073e36
ShevchenkoT <tarashevchuk@gmail.com>
2021-10-02 12:47:05 +0300

    home work lesson 4 part 3 bonus task is done

--- hw_lesson04.js
+++ hw_lesson04.js
@@ -9,7 +9,7 @@ function getTextFromArr(arr) {
     }).join('');
 }
 
- console.log(getTextFromArr([true, 3, 'Hello', false, 'mY', 253, 'NAME', null, 'tArAs'])); // output: helloMyNameTaras
+console.log(getTextFromArr([true, 3, 'Hello', false, 'mY', 253, 'NAME', null, 'tArAs'])); // output: helloMyNameTaras
 
 // get a char using a number
 function getCharUseNum(arr) {
@@ -19,7 +19,7 @@ function getCharUseNum(arr) {
     return null;
 }
 
- console.log(getCharUseNum([1, 3, 22, 11])); // output: [d, c, v, k]
+console.log(getCharUseNum([1, 3, 22, 11])); // output: [d, c, v, k]
 
 // filter the object by criteria
 function getNumberFromObj(obj) {
@@ -54,8 +54,8 @@ window.onload = () => {
 // call an additional window
 function beforeExit(){
     const exitDate = new Date();
-    let minutesInSite = ((exitDate - enterDate) / 1000).toFixed(0);
-    console.log(`Exit form page: ${exitDate.toLocaleString()}, You were on the page ${minutesInSite} minutes` );
+    let secondsInSite = ((exitDate - enterDate) / 1000).toFixed(0);
+    console.log(`Exit from page: ${exitDate.toLocaleString()}, You were on the page ${secondsInSite} seconds` );
     return 'some text...';
 }
 
@@ -106,14 +106,91 @@ yourInfo.printData(); // example expect: Your browser Google Chrome or Chromium,
 
 
 
+// Home work lesson 4 part 3 bonus task with star
+
+// return length of all rows in array
+function getLength(arr) {
+    return arr.map((el) =>
+        el.toString().split('').filter((letter) => {
+            const re = /^([a-zа-яё]+|\d+)$/i;
+            return re.test(letter);
+        }).join('').length)
+}
+
+const randArr = ['taras', ' She ', 'hello, my , name', 'age 25', 25, true, '2.5', ' '];
+// console.log(getLength(randArr)); // output: [5, 3, 11, 5, 2, 4, 2, 0]
+
+// convert array to object
+
+function fromArrToObj(arr) {
+    const newObj = {};
+    arr.map((el) => {
+        if (!Object.keys(newObj).includes(el[0])) {
+            newObj[el[0]] = el[1];
+        }
+    })
+    return newObj;
+}
+
+const testArr = [['a', 'hello'], ['b', 33], ['a', 'bye']];
+console.log(fromArrToObj(testArr)) // output: { a: 'hello', b: 33 }
+
+// convert query params to object
+function getObjFromQueryParams(str) {
+    const newObj = {};
+    str.substring(1).split('&').map((arr) => {
+        const [key, value] = arr.split('=');
+        newObj[key] = value;
+    })
+    return newObj;
+}
+
+const queryParams = '?a=22&b=33&c=44&d=55&e=111';
+console.log(getObjFromQueryParams(queryParams)); // output: { a: '22', b: '33', c: '44', d: '55', e: '111' }
 
 
+// get the most nested elements of the array
+function getDeepestVariables(arr) {
+    while (arr.some((el) => Array.isArray(el))) {
+        arr = arr.filter((el) => typeof el !== 'number').flat(1);
+    }
+    return arr;
+}
 
+const arrInArr = [1, [2, 3], [[4], 5], [[6]]];
+console.log(getDeepestVariables(arrInArr)); // output: [ 4, 6 ]
 
+// deepen the variable by its value
+function goToDeep(arr) {
+    function deep(idx, i) {
+        if (idx > 0) {
+            const newArr = new Array();
+            newArr[0] = deep(--idx, i);
+            return newArr;
+        }
+        return i;
+    }
+    const newArr = [];
 
+    arr.map((ar) => {
+        newArr.push(deep(ar, ar));
+    })
+    return newArr;
+}
 
+const someArr = [1, 2, 1, 3, 4]; 
+console.log(goToDeep(someArr)); // output: [ [ 1 ], [ [ 2 ] ], [ 1 ], [ [ [ 3 ] ] ], [ [ [ [ 4 ] ] ] ] ]
 
+// convert selected key values ​​to a number
+function convertToNumber(data, fieldsToNumber) {
+    const newObj = {};
+    for (let [key, val] of Object.entries(data)) {
+        fieldsToNumber.includes(key) ? newObj[key] = Number.parseFloat(val.toString().replace(',', '.')) : newObj[key] = val;
+    }
+    return newObj;
+}
 
-
-
+const data = { a: '21', b: 'sensor', c:'0,2', d: '00.10' };
+const fieldsToNumber = ['a', 'c'];
+console.log(convertToNumber(data, fieldsToNumber)); // output: { a: 21, b: 'sensor', c: 0.2, d: '00.10' }
 
