def2ac057881c8ad2bcc11d104b9fbff3de61faf
ShevchenkoT <tarashevchuk@gmail.com>
2021-10-31 03:23:13 +0300

    added contextmenu pencil, and eraser

--- testWork04Paint/app.js
+++ testWork04Paint/app.js
@@ -4,21 +4,35 @@ const context = canvas.getContext("2d");
 const colorPanel = document.querySelector("#colors");
 
 const lineWidth = document.querySelector("#lineWidth");
-const lineColor = document.querySelector("#lineColor");
+const mainBtnColor = document.querySelector("#main-bnt-color");
+const contextMenuBtnColor = document.querySelector("#context-menu-bnt-color");
 const cleanCanvasBtn = document.querySelector("#clean-canvas");
+const eraser = document.querySelector("#eraser");
+const pencil = document.querySelector("#pencil");
 
 let isMouseDown = false;
 let coordinates = [];
 
 const properties = {
   width: 1,
-  color: "#000000",
+  mainBtnColor: "#000000",
+  contextBtnColor: "#ffffff",
+  currentColor: "#000000",
+  instrumentType: {
+    pencil: true,
+    eraser: false,
+  },
 };
 
 canvas.width = 750;
 canvas.height = 500;
 
-canvas.addEventListener("mousedown", () => {
+canvas.addEventListener("mousedown", (event) => {
+  if (properties.instrumentType.pencil) {
+    properties.currentColor =
+      event.button === 2 ? properties.contextBtnColor : properties.mainBtnColor;
+  }
+
   isMouseDown = true;
 });
 
@@ -32,17 +46,24 @@ canvas.addEventListener("mousemove", (event) => {
     coordinates.push([event.offsetX, event.offsetY]);
     context.lineTo(event.offsetX, event.offsetY);
     context.lineWidth = properties.width;
-    context.strokeStyle = properties.color;
+    context.strokeStyle = properties.currentColor;
     context.stroke();
   }
+});
+canvas.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
 });
 
 lineWidth.addEventListener("change", (event) => {
   properties.width = event.target.value;
 });
 
-lineColor.addEventListener("change", (event) => {
-  properties.color = event.target.value;
+mainBtnColor.addEventListener("change", (event) => {
+  properties.mainBtnColor = event.target.value;
+});
+
+contextMenuBtnColor.addEventListener("change", (event) => {
+  properties.contextBtnColor = event.target.value;
 });
 
 cleanCanvasBtn.addEventListener("click", (event) => {
@@ -52,16 +73,51 @@ cleanCanvasBtn.addEventListener("click", (event) => {
 colorPanel.addEventListener("click", (event) => {
   if (event.target.classList.contains("colors")) return;
   const elBg = event.target.style.backgroundColor;
-  properties.color = elBg;
-  //lineColor.value = rgbToHex(elBg);
+  properties.mainBtnColor = elBg;
+  mainBtnColor.value = rgbToHex(elBg);
 });
 
-// function componentToHex(c) {
-//   var hex = c.toString(16);
+//!
+colorPanel.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("colors")) return;
+  const elBg = event.target.style.backgroundColor;
+  properties.contextBtnColor = elBg;
+  contextMenuBtnColor.value = rgbToHex(elBg);
+});
+//!
 
-//   return hex.length == 1 ? "0" + hex : hex;
-// }
-// function rgbToHex(color) {
+// tools
+eraser.addEventListener("click", () => {
+  changeCurrentInstrument("eraser");
+  properties.currentColor = "#ffffff";
+  properties.width = 30;
+  canvas.classList.add("eraser-cursor");
+});
 
-//   //return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-// }
+pencil.addEventListener("click", () => {
+  properties.width = lineWidth.value;
+  changeCurrentInstrument("pencil");
+  canvas.classList.remove("eraser-cursor");
+});
+
+function rgbToHex(color) {
+  function toHex(c) {
+    const hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+  }
+  //!DEBUG
+  color = color
+    .replace(/[a-z()\s]/g, "")
+    .split(",")
+    .map((c) => +c);
+
+  return `#${toHex(color[0])}${toHex(color[1])}${toHex(color[2])}`;
+}
+
+function changeCurrentInstrument(currentInstrument) {
+  for (let key in properties.instrumentType) {
+    properties.instrumentType[key] = false;
+  }
+  properties.instrumentType[currentInstrument] = true;
+}
